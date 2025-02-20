468956353060f4c3d7e103c952343ba081c013c7
MowiliMi <38068391+MowiliMi@users.noreply.github.com>
2025-02-20 09:59:26 +0100

    fix router types

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,10 @@
-import { Router, type Request, type Response, type NextFunction } from 'express';
+import type { Request, Response, NextFunction } from "express";
+import { Router } from "express";
 
 const router: Router = Router();
 
-router.use('*', (req: Request, res: Response, next: NextFunction) => res.status(301).redirect('/'));
+router.use("*", (req: Request, res: Response, next: NextFunction) =>
+  res.status(301).redirect("/")
+);
 
 export default router;
